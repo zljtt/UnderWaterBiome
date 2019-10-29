9776db4c458f3b368c8251b04606f07162e99534
zljtt <johnwang2020@163.com>
2019-10-29 13:13:56 +0800

    fix bug

--- main/java/com/github/zljtt/underwaterbiome/Handlers/TriggerHandler.java
+++ main/java/com/github/zljtt/underwaterbiome/Handlers/TriggerHandler.java
@@ -13,7 +13,7 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class TriggerHandler 
 {
-	private static Method CriterionRegister;
+//	private static Method CriterionRegister;
 	public static final TriggerUnlockBluePrint UNLOCKED_BLUE_PRINT = new TriggerUnlockBluePrint("unlocked_blue_print");
 	public static final TriggerNeedTechPoint NEED_TECHPOINT = new TriggerNeedTechPoint("have_knowledge");
 
@@ -23,32 +23,40 @@ public class TriggerHandler
     				NEED_TECHPOINT
             };
     
-    @SuppressWarnings("unchecked")
-	public static <T extends ICriterionInstance> ICriterionTrigger<T> registerAdvancementTrigger(ICriterionTrigger<T> trigger) 
-    {
-		if(CriterionRegister == null) 
-		{
-			CriterionRegister = ObfuscationReflectionHelper.findMethod(CriteriaTriggers.class, "register", ICriterionTrigger.class);
-			CriterionRegister.setAccessible(true);
-		}
-		try 
-		{
-			System.out.println("Succeeded to register trigger " + trigger.getId());
-			trigger = (ICriterionTrigger<T>) CriterionRegister.invoke(null, trigger);
-		} 
-		catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) 
-		{
-			System.out.println("Failed to register trigger " + trigger.getId());
-			e.printStackTrace();
-		}
-		return trigger;
-	}
+//    @SuppressWarnings("unchecked")
+//	public static <T extends ICriterionInstance> ICriterionTrigger<T> registerAdvancementTrigger(ICriterionTrigger<T> trigger) 
+//    {
+//    	CriteriaTriggers.register(trigger);
+//		if(CriterionRegister == null) 
+//		{
+//			for (Method m:CriteriaTriggers.class.getDeclaredMethods())
+//			{
+//				System.out.print(m.getName());
+//			}
+//			try {
+//				CriterionRegister = ObfuscationReflectionHelper.findMethod(CriteriaTriggers.class, "register", ICriterionTrigger.class);
+//			}catch(Exception e) {System.out.println("no method 1");}
+//			CriterionRegister.setAccessible(true);
+//		}
+//		try 
+//		{
+//			System.out.println("Succeeded to register trigger " + trigger.getId());
+//			trigger = (ICriterionTrigger<T>) CriterionRegister.invoke(null, trigger);
+//		} 
+//		catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) 
+//		{
+//			System.out.println("Failed to register trigger " + trigger.getId());
+//			e.printStackTrace();
+//		}
+//		return trigger;
+//	}
 
 	public static void init() 
 	{
 		 for (ICriterionTrigger<?> trigger: TriggerHandler.TRIGGER_ARRAY)
 		 {
-			 registerAdvancementTrigger(trigger);
+		    	CriteriaTriggers.register(trigger);
+//			 registerAdvancementTrigger(trigger);
 	     }		
 	}
 	
